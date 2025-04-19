83556e4859d14f703f2a9290ce0b1fc2e7dd1fe5
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-04-19 15:17:10 +0800

    api view for backend established

--- Project/backend/urls.py
+++ Project/backend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'backend' # insert app name
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- Project/backend/views.py
+++ Project/backend/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET', 'POST'])
 def index(request):
-    return JsonResponse({'message': 'backend'})
+    if request.method == 'GET':
+        return Response({'message': 'api view'}, status=200)
+    
+    elif request.method == 'POST':
+        return Response(request.data, status=200)
